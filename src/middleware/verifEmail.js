f2074776ca00ce6e4ff4e9131a68f9a895d71d9c
Mahardhika Pratama <putrad578@gmail.com>
2023-10-13 18:55:33 +0700

    restyle design email push notif registration

--- src/middleware/verifEmail.js
+++ src/middleware/verifEmail.js
@@ -1,44 +1,44 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-    host: "mail.project13.my.id",
-    secure: true,
-    port: 465,
-    auth: {
-        user: process.env.EMAIL_NAME,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: 'mail.project13.my.id',
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_NAME,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const sendEmail = async (email_client, name, url, code) => {
-    let mailOption = {
-        from: `"Recipe App" <${process.env.EMAIL_NAME}>`,
-        to: email_client,
-        subject: `Activation email for ${name}`,
-        html: `
-        <div style="text-align: center;">
+  let mailOption = {
+    from: `"Recipe App" <${process.env.EMAIL_NAME}>`,
+    to: email_client,
+    subject: `Activation email for ${name}`,
+    html: `
+        <div style="margin: 50px auto; width: 400px; background-color: #fff8e7; padding: 15px;border-radius: 15px; box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);">
         <img src="https://i.ibb.co/ZcsX3g3/fix.png" alt="Logo Recipe" width="150" height="150" style="display: block; margin: 0 auto;">
-        </div>
+        <h3>Hello, <b>${name}</b></h3>
+        <p>Registration at <strong>Mama Recipe</strong> was successful, here is the code to activate your email, don't give this code to anyone:</p>
+        <div style="background-color: #EFC81A; border-radius: 10px;padding: 10px; color: white;">${code}</div>
+        <p>Or you can click this link to activating your email ${url}.</p>
+        <p>If you feel you have not registered for our application/web service, please ignore this email.</p>
         <br>
-        Hello <b>${name}</b>,<br>
-        Here is the code to activate Email, copy this:<br>
-        <blockquote>
-        <span style="background-color: yellow"><strong>${code}</strong></span>
-        </blockquote><br>
-        Or you can click this link to activate email <i><a href="${url}">click here</a></i>.<br>
-        Thank you for your trust in using our services.<br>
-        <strong>Mahardhika Putra P</strong>
+        <p>Best regards,</p>
+        <strong>Mahardhika Putra Pratama</strong>
+        <p style="padding: 0;margin: 0;">(Administrator)</p>
+        </div>
         `,
-    };
+  };
 
-    try {
-        let info = await transporter.sendMail(mailOption);
-        console.log("Email sent:", info.response);
-        return info.response;
-    } catch (error) {
-        console.log("Error sending email:", error);
-        throw error;
-    }
+  try {
+    let info = await transporter.sendMail(mailOption);
+    console.log('Email sent:', info.response);
+    return info.response;
+  } catch (error) {
+    console.log('Error sending email:', error);
+    throw error;
+  }
 };
 
-module.exports = sendEmail
+module.exports = sendEmail;
